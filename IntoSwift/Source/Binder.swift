7b78a96dad210ad76a511ed5d4793bf9e3dafd10
Nisse Bergman <nisse@potmo.com>
2016-03-09 22:53:45 +0100

    add bindings with up to 20 arguments

--- IntoSwift/Source/Binder.swift
+++ IntoSwift/Source/Binder.swift
@@ -29,25 +29,6 @@ class Binder {
         bind(interface, toConstructor: constructor, dependencies: [], inScope: scope)
     }
     
-    func bind<T, P, A0>(interface: P.Type, toConstructor constructor: (A0) throws -> T, inScope scope: Scope) -> Void {
-        let wrapperConstructor = { () throws -> T in
-            let argument0 = try self.resolver.tryResolve(A0.self)
-            let instance = try constructor(argument0)
-            return instance
-        }
-        bind(interface, toConstructor: wrapperConstructor, dependencies: [A0.self], inScope: scope)
-    }
-    
-    func bind<T, P, A0, A1>(interface: P.Type, toConstructor constructor: (A0, A1) throws -> T, inScope scope: Scope) -> Void {
-        let wrapperConstructor = { () throws -> T in
-            let argument0 = try self.resolver.tryResolve(A0.self)
-            let argument1 = try self.resolver.tryResolve(A1.self)
-            let instance = try constructor(argument0, argument1)
-            return instance
-        }
-        bind(interface, toConstructor: wrapperConstructor, dependencies: [A0.self, A1.self], inScope: scope)
-    }
-    
     func bind<T, P>(interface: P.Type, toConstructor constructor: () throws -> T, dependencies: [Any.Type], inScope scope: Scope) -> Void {
         
         let protocolConstructor = { () throws -> P in
@@ -81,4 +62,178 @@ class Binder {
     func build() -> (proxy: ResolverProxy, bindings: [Binding]){
         return (resolver, bindings)
     }
+}
+
+extension Binder {
+    
+    func bind<T, P, A0>(interface: P.Type, toConstructor constructor: (A0) throws -> T, inScope scope: Scope) -> Void {
+        let r = resolver
+        let wrapperConstructor = { () throws -> T in
+            return try constructor(r.tryResolve(A0.self))
+        }
+        bind(interface, toConstructor: wrapperConstructor, dependencies: [A0.self], inScope: scope)
+    }
+    
+    func bind<T, P, A0, A1>(interface: P.Type, toConstructor constructor: (A0, A1) throws -> T, inScope scope: Scope) -> Void {
+        let r = resolver
+        let wrapperConstructor = { () throws -> T in
+            return try constructor(r.tryResolve(A0.self), r.tryResolve(A1.self))
+        }
+        bind(interface, toConstructor: wrapperConstructor, dependencies: [A0.self, A1.self], inScope: scope)
+    }
+    
+    
+    func bind<T, P, A0, A1, A2>(interface: P.Type, toConstructor constructor: (A0, A1, A2) throws -> T, inScope scope: Scope) -> Void {
+        let r = resolver
+        let wrapperConstructor = { () throws -> T in
+            return try constructor(r.tryResolve(A0.self), r.tryResolve(A1.self), r.tryResolve(A2.self))
+        }
+        bind(interface, toConstructor: wrapperConstructor, dependencies: [A0.self, A1.self, A2.self], inScope: scope)
+    }
+    
+    func bind<T, P, A0, A1, A2, A3>(interface: P.Type, toConstructor constructor: (A0, A1, A2, A3) throws -> T, inScope scope: Scope) -> Void {
+        let r = resolver
+        let wrapperConstructor = { () throws -> T in
+            return try constructor(r.tryResolve(A0.self), r.tryResolve(A1.self), r.tryResolve(A2.self), r.tryResolve(A3.self))
+        }
+        bind(interface, toConstructor: wrapperConstructor, dependencies: [A0.self, A1.self, A2.self, A3.self], inScope: scope)
+    }
+    
+    func bind<T, P, A0, A1, A2, A3, A4>(interface: P.Type, toConstructor constructor: (A0, A1, A2, A3, A4) throws -> T, inScope scope: Scope) -> Void {
+        let r = resolver
+        let wrapperConstructor = { () throws -> T in
+            return try constructor(r.tryResolve(A0.self), r.tryResolve(A1.self), r.tryResolve(A2.self), r.tryResolve(A3.self), r.tryResolve(A4.self))
+        }
+        bind(interface, toConstructor: wrapperConstructor, dependencies: [A0.self, A1.self, A2.self, A3.self, A4.self], inScope: scope)
+    }
+    
+    func bind<T, P, A0, A1, A2, A3, A4, A5>(interface: P.Type, toConstructor constructor: (A0, A1, A2, A3, A4, A5) throws -> T, inScope scope: Scope) -> Void {
+        let r = resolver
+        let wrapperConstructor = { () throws -> T in
+            return try constructor(r.tryResolve(A0.self), r.tryResolve(A1.self), r.tryResolve(A2.self), r.tryResolve(A3.self), r.tryResolve(A4.self), r.tryResolve(A5.self))
+        }
+        bind(interface, toConstructor: wrapperConstructor, dependencies: [A0.self, A1.self, A2.self, A3.self, A4.self, A5.self], inScope: scope)
+    }
+    
+    func bind<T, P, A0, A1, A2, A3, A4, A5, A6>(interface: P.Type, toConstructor constructor: (A0, A1, A2, A3, A4, A5, A6) throws -> T, inScope scope: Scope) -> Void {
+        let r = resolver
+        let wrapperConstructor = { () throws -> T in
+            return try constructor(r.tryResolve(A0.self), r.tryResolve(A1.self), r.tryResolve(A2.self), r.tryResolve(A3.self), r.tryResolve(A4.self), r.tryResolve(A5.self), r.tryResolve(A6.self))
+        }
+        bind(interface, toConstructor: wrapperConstructor, dependencies: [A0.self, A1.self, A2.self, A3.self, A4.self, A5.self, A6.self], inScope: scope)
+    }
+    
+    func bind<T, P, A0, A1, A2, A3, A4, A5, A6, A7>(interface: P.Type, toConstructor constructor: (A0, A1, A2, A3, A4, A5, A6, A7) throws -> T, inScope scope: Scope) -> Void {
+        let r = resolver
+        let wrapperConstructor = { () throws -> T in
+            return try constructor(r.tryResolve(A0.self), r.tryResolve(A1.self), r.tryResolve(A2.self), r.tryResolve(A3.self), r.tryResolve(A4.self), r.tryResolve(A5.self), r.tryResolve(A6.self), r.tryResolve(A7.self))
+        }
+        bind(interface, toConstructor: wrapperConstructor, dependencies: [A0.self, A1.self, A2.self, A3.self, A4.self, A5.self, A6.self, A7.self], inScope: scope)
+    }
+    
+    func bind<T, P, A0, A1, A2, A3, A4, A5, A6, A7, A8>(interface: P.Type, toConstructor constructor: (A0, A1, A2, A3, A4, A5, A6, A7, A8) throws -> T, inScope scope: Scope) -> Void {
+        let r = resolver
+        let wrapperConstructor = { () throws -> T in
+            return try constructor(r.tryResolve(A0.self), r.tryResolve(A1.self), r.tryResolve(A2.self), r.tryResolve(A3.self), r.tryResolve(A4.self), r.tryResolve(A5.self), r.tryResolve(A6.self), r.tryResolve(A7.self), r.tryResolve(A8.self))
+        }
+        bind(interface, toConstructor: wrapperConstructor, dependencies: [A0.self, A1.self, A2.self, A3.self, A4.self, A5.self, A6.self, A7.self, A8.self], inScope: scope)
+    }
+    
+    func bind<T, P, A0, A1, A2, A3, A4, A5, A6, A7, A8, A9>(interface: P.Type, toConstructor constructor: (A0, A1, A2, A3, A4, A5, A6, A7, A8, A9) throws -> T, inScope scope: Scope) -> Void {
+        let r = resolver
+        let wrapperConstructor = { () throws -> T in
+            return try constructor(r.tryResolve(A0.self), r.tryResolve(A1.self), r.tryResolve(A2.self), r.tryResolve(A3.self), r.tryResolve(A4.self), r.tryResolve(A5.self), r.tryResolve(A6.self), r.tryResolve(A7.self), r.tryResolve(A8.self), r.tryResolve(A9.self))
+        }
+        bind(interface, toConstructor: wrapperConstructor, dependencies: [A0.self, A1.self, A2.self, A3.self, A4.self, A5.self, A6.self, A7.self, A8.self, A9.self], inScope: scope)
+    }
+    
+    func bind<T, P, A0, A1, A2, A3, A4, A5, A6, A7, A8, A9, A10>(interface: P.Type, toConstructor constructor: (A0, A1, A2, A3, A4, A5, A6, A7, A8, A9, A10) throws -> T, inScope scope: Scope) -> Void {
+        let r = resolver
+        let wrapperConstructor = { () throws -> T in
+            return try constructor(r.tryResolve(A0.self), r.tryResolve(A1.self), r.tryResolve(A2.self), r.tryResolve(A3.self), r.tryResolve(A4.self), r.tryResolve(A5.self), r.tryResolve(A6.self), r.tryResolve(A7.self), r.tryResolve(A8.self), r.tryResolve(A9.self),  r.tryResolve(A10.self))
+        }
+        bind(interface, toConstructor: wrapperConstructor, dependencies: [A0.self, A1.self, A2.self, A3.self, A4.self, A5.self, A6.self, A7.self, A8.self, A9.self, A10.self], inScope: scope)
+    }
+    
+    func bind<T, P, A0, A1, A2, A3, A4, A5, A6, A7, A8, A9, A10, A11>(interface: P.Type, toConstructor constructor: (A0, A1, A2, A3, A4, A5, A6, A7, A8, A9, A10, A11) throws -> T, inScope scope: Scope) -> Void {
+        let r = resolver
+        let wrapperConstructor = { () throws -> T in
+            return try constructor(r.tryResolve(A0.self), r.tryResolve(A1.self), r.tryResolve(A2.self), r.tryResolve(A3.self), r.tryResolve(A4.self), r.tryResolve(A5.self), r.tryResolve(A6.self), r.tryResolve(A7.self), r.tryResolve(A8.self), r.tryResolve(A9.self), r.tryResolve(A10.self), r.tryResolve(A11.self))
+        }
+        bind(interface, toConstructor: wrapperConstructor, dependencies: [A0.self, A1.self, A2.self, A3.self, A4.self, A5.self, A6.self, A7.self, A8.self, A9.self, A10.self, A11.self], inScope: scope)
+    }
+    
+    func bind<T, P, A0, A1, A2, A3, A4, A5, A6, A7, A8, A9, A10, A11, A12>(interface: P.Type, toConstructor constructor: (A0, A1, A2, A3, A4, A5, A6, A7, A8, A9, A10, A11, A12) throws -> T, inScope scope: Scope) -> Void {
+        let r = resolver
+        let wrapperConstructor = { () throws -> T in
+            return try constructor(r.tryResolve(A0.self), r.tryResolve(A1.self), r.tryResolve(A2.self), r.tryResolve(A3.self), r.tryResolve(A4.self), r.tryResolve(A5.self), r.tryResolve(A6.self), r.tryResolve(A7.self), r.tryResolve(A8.self), r.tryResolve(A9.self), r.tryResolve(A10.self), r.tryResolve(A11.self), r.tryResolve(A12.self))
+        }
+        bind(interface, toConstructor: wrapperConstructor, dependencies: [A0.self, A1.self, A2.self, A3.self, A4.self, A5.self, A6.self, A7.self, A8.self, A9.self, A10.self, A11.self, A12.self], inScope: scope)
+    }
+    
+    func bind<T, P, A0, A1, A2, A3, A4, A5, A6, A7, A8, A9, A10, A11, A12, A13>(interface: P.Type, toConstructor constructor: (A0, A1, A2, A3, A4, A5, A6, A7, A8, A9, A10, A11, A12, A13) throws -> T, inScope scope: Scope) -> Void {
+        let r = resolver
+        let wrapperConstructor = { () throws -> T in
+            return try constructor(r.tryResolve(A0.self), r.tryResolve(A1.self), r.tryResolve(A2.self), r.tryResolve(A3.self), r.tryResolve(A4.self), r.tryResolve(A5.self), r.tryResolve(A6.self), r.tryResolve(A7.self), r.tryResolve(A8.self), r.tryResolve(A9.self), r.tryResolve(A10.self), r.tryResolve(A11.self), r.tryResolve(A12.self), r.tryResolve(A13.self))
+        }
+        bind(interface, toConstructor: wrapperConstructor, dependencies: [A0.self, A1.self, A2.self, A3.self, A4.self, A5.self, A6.self, A7.self, A8.self, A9.self, A10.self, A11.self, A12.self, A13.self], inScope: scope)
+    }
+    
+    func bind<T, P, A0, A1, A2, A3, A4, A5, A6, A7, A8, A9, A10, A11, A12, A13, A14>(interface: P.Type, toConstructor constructor: (A0, A1, A2, A3, A4, A5, A6, A7, A8, A9, A10, A11, A12, A13, A14) throws -> T, inScope scope: Scope) -> Void {
+        let r = resolver
+        let wrapperConstructor = { () throws -> T in
+            return try constructor(r.tryResolve(A0.self), r.tryResolve(A1.self), r.tryResolve(A2.self), r.tryResolve(A3.self), r.tryResolve(A4.self), r.tryResolve(A5.self), r.tryResolve(A6.self), r.tryResolve(A7.self), r.tryResolve(A8.self), r.tryResolve(A9.self), r.tryResolve(A10.self), r.tryResolve(A11.self), r.tryResolve(A12.self), r.tryResolve(A13.self), r.tryResolve(A14.self))
+        }
+        bind(interface, toConstructor: wrapperConstructor, dependencies: [A0.self, A1.self, A2.self, A3.self, A4.self, A5.self, A6.self, A7.self, A8.self, A9.self, A10.self, A11.self, A12.self, A13.self, A14.self], inScope: scope)
+    }
+    
+    func bind<T, P, A0, A1, A2, A3, A4, A5, A6, A7, A8, A9, A10, A11, A12, A13, A14, A15>(interface: P.Type, toConstructor constructor: (A0, A1, A2, A3, A4, A5, A6, A7, A8, A9, A10, A11, A12, A13, A14, A15) throws -> T, inScope scope: Scope) -> Void {
+        let r = resolver
+        let wrapperConstructor = { () throws -> T in
+            return try constructor(r.tryResolve(A0.self), r.tryResolve(A1.self), r.tryResolve(A2.self), r.tryResolve(A3.self), r.tryResolve(A4.self), r.tryResolve(A5.self), r.tryResolve(A6.self), r.tryResolve(A7.self), r.tryResolve(A8.self), r.tryResolve(A9.self), r.tryResolve(A10.self), r.tryResolve(A11.self), r.tryResolve(A12.self), r.tryResolve(A13.self), r.tryResolve(A14.self), r.tryResolve(A15.self))
+        }
+        bind(interface, toConstructor: wrapperConstructor, dependencies: [A0.self, A1.self, A2.self, A3.self, A4.self, A5.self, A6.self, A7.self, A8.self, A9.self, A10.self, A11.self, A12.self, A13.self, A14.self, A15.self], inScope: scope)
+    }
+    
+    func bind<T, P, A0, A1, A2, A3, A4, A5, A6, A7, A8, A9, A10, A11, A12, A13, A14, A15, A16>(interface: P.Type, toConstructor constructor: (A0, A1, A2, A3, A4, A5, A6, A7, A8, A9, A10, A11, A12, A13, A14, A15, A16) throws -> T, inScope scope: Scope) -> Void {
+        let r = resolver
+        let wrapperConstructor = { () throws -> T in
+            return try constructor(r.tryResolve(A0.self), r.tryResolve(A1.self), r.tryResolve(A2.self), r.tryResolve(A3.self), r.tryResolve(A4.self), r.tryResolve(A5.self), r.tryResolve(A6.self), r.tryResolve(A7.self), r.tryResolve(A8.self), r.tryResolve(A9.self), r.tryResolve(A10.self), r.tryResolve(A11.self), r.tryResolve(A12.self), r.tryResolve(A13.self), r.tryResolve(A14.self), r.tryResolve(A15.self), r.tryResolve(A16.self))
+        }
+        bind(interface, toConstructor: wrapperConstructor, dependencies: [A0.self, A1.self, A2.self, A3.self, A4.self, A5.self, A6.self, A7.self, A8.self, A9.self, A10.self, A11.self, A12.self, A13.self, A14.self, A15.self, A16.self], inScope: scope)
+    }
+    
+    func bind<T, P, A0, A1, A2, A3, A4, A5, A6, A7, A8, A9, A10, A11, A12, A13, A14, A15, A16, A17>(interface: P.Type, toConstructor constructor: (A0, A1, A2, A3, A4, A5, A6, A7, A8, A9, A10, A11, A12, A13, A14, A15, A16, A17) throws -> T, inScope scope: Scope) -> Void {
+        let r = resolver
+        let wrapperConstructor = { () throws -> T in
+            return try constructor(r.tryResolve(A0.self), r.tryResolve(A1.self), r.tryResolve(A2.self), r.tryResolve(A3.self), r.tryResolve(A4.self), r.tryResolve(A5.self), r.tryResolve(A6.self), r.tryResolve(A7.self), r.tryResolve(A8.self), r.tryResolve(A9.self), r.tryResolve(A10.self), r.tryResolve(A11.self), r.tryResolve(A12.self), r.tryResolve(A13.self), r.tryResolve(A14.self), r.tryResolve(A15.self), r.tryResolve(A16.self), r.tryResolve(A17.self))
+        }
+        bind(interface, toConstructor: wrapperConstructor, dependencies: [A0.self, A1.self, A2.self, A3.self, A4.self, A5.self, A6.self, A7.self, A8.self, A9.self, A10.self, A11.self, A12.self, A13.self, A14.self, A15.self, A16.self, A17.self], inScope: scope)
+    }
+    
+    func bind<T, P, A0, A1, A2, A3, A4, A5, A6, A7, A8, A9, A10, A11, A12, A13, A14, A15, A16, A17, A18>(interface: P.Type, toConstructor constructor: (A0, A1, A2, A3, A4, A5, A6, A7, A8, A9, A10, A11, A12, A13, A14, A15, A16, A17, A18) throws -> T, inScope scope: Scope) -> Void {
+        let r = resolver
+        let wrapperConstructor = { () throws -> T in
+            return try constructor(r.tryResolve(A0.self), r.tryResolve(A1.self), r.tryResolve(A2.self), r.tryResolve(A3.self), r.tryResolve(A4.self), r.tryResolve(A5.self), r.tryResolve(A6.self), r.tryResolve(A7.self), r.tryResolve(A8.self), r.tryResolve(A9.self), r.tryResolve(A10.self), r.tryResolve(A11.self), r.tryResolve(A12.self), r.tryResolve(A13.self), r.tryResolve(A14.self), r.tryResolve(A15.self), r.tryResolve(A16.self), r.tryResolve(A17.self), r.tryResolve(A18.self))
+        }
+        bind(interface, toConstructor: wrapperConstructor, dependencies: [A0.self, A1.self, A2.self, A3.self, A4.self, A5.self, A6.self, A7.self, A8.self, A9.self, A10.self, A11.self, A12.self, A13.self, A14.self, A15.self, A16.self, A17.self, A18.self], inScope: scope)
+    }
+    
+    func bind<T, P, A0, A1, A2, A3, A4, A5, A6, A7, A8, A9, A10, A11, A12, A13, A14, A15, A16, A17, A18, A19>(interface: P.Type, toConstructor constructor: (A0, A1, A2, A3, A4, A5, A6, A7, A8, A9, A10, A11, A12, A13, A14, A15, A16, A17, A18, A19) throws -> T, inScope scope: Scope) -> Void {
+        let r = resolver
+        let wrapperConstructor = { () throws -> T in
+            return try constructor(r.tryResolve(A0.self), r.tryResolve(A1.self), r.tryResolve(A2.self), r.tryResolve(A3.self), r.tryResolve(A4.self), r.tryResolve(A5.self), r.tryResolve(A6.self), r.tryResolve(A7.self), r.tryResolve(A8.self), r.tryResolve(A9.self), r.tryResolve(A10.self), r.tryResolve(A11.self), r.tryResolve(A12.self), r.tryResolve(A13.self), r.tryResolve(A14.self), r.tryResolve(A15.self), r.tryResolve(A16.self), r.tryResolve(A17.self), r.tryResolve(A18.self), r.tryResolve(A19.self))
+        }
+        bind(interface, toConstructor: wrapperConstructor, dependencies: [A0.self, A1.self, A2.self, A3.self, A4.self, A5.self, A6.self, A7.self, A8.self, A9.self, A10.self, A11.self, A12.self, A13.self, A14.self, A15.self, A16.self, A17.self, A18.self, A19.self], inScope: scope)
+    }
+    
+    func bind<T, P, A0, A1, A2, A3, A4, A5, A6, A7, A8, A9, A10, A11, A12, A13, A14, A15, A16, A17, A18, A19, A20>(interface: P.Type, toConstructor constructor: (A0, A1, A2, A3, A4, A5, A6, A7, A8, A9, A10, A11, A12, A13, A14, A15, A16, A17, A18, A19, A20) throws -> T, inScope scope: Scope) -> Void {
+        let r = resolver
+        let wrapperConstructor = { () throws -> T in
+            return try constructor(r.tryResolve(A0.self), r.tryResolve(A1.self), r.tryResolve(A2.self), r.tryResolve(A3.self), r.tryResolve(A4.self), r.tryResolve(A5.self), r.tryResolve(A6.self), r.tryResolve(A7.self), r.tryResolve(A8.self), r.tryResolve(A9.self), r.tryResolve(A10.self), r.tryResolve(A11.self), r.tryResolve(A12.self), r.tryResolve(A13.self), r.tryResolve(A14.self), r.tryResolve(A15.self), r.tryResolve(A16.self), r.tryResolve(A17.self), r.tryResolve(A18.self), r.tryResolve(A19.self), r.tryResolve(A20.self))
+        }
+        bind(interface, toConstructor: wrapperConstructor, dependencies: [A0.self, A1.self, A2.self, A3.self, A4.self, A5.self, A6.self, A7.self, A8.self, A9.self, A10.self, A11.self, A12.self, A13.self, A14.self, A15.self, A16.self, A17.self, A18.self, A19.self, A20.self], inScope: scope)
+    }
+
+
 }
--- IntoSwift/Source/Injector.swift
+++ IntoSwift/Source/Injector.swift
@@ -1,37 +1,3 @@
-
-public struct OngoingBinding<T>{
-    private let injector: Injector
-    private let protocolType: T.Type
-    private let scope: Scope
-
-    func to(constructor: () throws -> T) -> Injector{
-        injector.binder.bind(protocolType, toConstructor: constructor, inScope: scope)
-        return injector
-    }
-    
-    func to<A0>(constructor: (A0) throws -> T) -> Injector{
-        injector.binder.bind(protocolType, toConstructor: constructor, inScope: scope)
-        return injector
-    }
-    
-    func to<A0, A1>(constructor: (A0, A1) throws -> T) -> Injector{
-        injector.binder.bind(protocolType, toConstructor: constructor, inScope: scope)
-        return injector
-    }
-    
-    //TODO: Add more functions for 3,4,5 etc dependency arguments
-}
-
-public struct ScopeBinding {
-    private let injector: Injector
-    private let scope: Scope
-    
-    func bind<T>(type: T.Type) -> OngoingBinding<T> {
-        return OngoingBinding<T>(injector: injector, protocolType: T.self, scope: scope)
-    }
-}
-
-
 public class Injector {
     
     private var binder: Binder
@@ -84,5 +50,130 @@ public class Injector {
         let (_, bindings) = binder.build()
         let graph = graphChecker.resolve(bindings)
         return graph.prettyPrint()
+    }
+}
+
+public struct ScopeBinding {
+    private let injector: Injector
+    private let scope: Scope
+    
+    public func bind<T>(type: T.Type) -> OngoingBinding<T> {
+        return OngoingBinding<T>(injector: injector, protocolType: T.self, scope: scope)
+    }
+}
+
+public struct OngoingBinding<T>{
+    private let injector: Injector
+    private let protocolType: T.Type
+    private let scope: Scope
+    
+    public func to(constructor: () throws -> T) -> Injector{
+        injector.binder.bind(protocolType, toConstructor: constructor, inScope: scope)
+        return injector
+    }
+    
+    public func to<A0>(constructor: (A0) throws -> T) -> Injector{
+        injector.binder.bind(protocolType, toConstructor: constructor, inScope: scope)
+        return injector
+    }
+    
+    public func to<A0, A1>(constructor: (A0, A1) throws -> T) -> Injector{
+        injector.binder.bind(protocolType, toConstructor: constructor, inScope: scope)
+        return injector
+    }
+    
+    public func to<A0, A1, A2>(constructor: (A0, A1, A2) throws -> T) -> Injector{
+        injector.binder.bind(protocolType, toConstructor: constructor, inScope: scope)
+        return injector
+    }
+    
+    public func to<A0, A1, A2, A3>(constructor: (A0, A1, A2, A3) throws -> T) -> Injector{
+        injector.binder.bind(protocolType, toConstructor: constructor, inScope: scope)
+        return injector
+    }
+    
+    public func to<A0, A1, A2, A3, A4>(constructor: (A0, A1, A2, A3, A4) throws -> T) -> Injector{
+        injector.binder.bind(protocolType, toConstructor: constructor, inScope: scope)
+        return injector
+    }
+    
+    public func to<A0, A1, A2, A3, A4, A5>(constructor: (A0, A1, A2, A3, A4, A5) throws -> T) -> Injector{
+        injector.binder.bind(protocolType, toConstructor: constructor, inScope: scope)
+        return injector
+    }
+    
+    public func to<A0, A1, A2, A3, A4, A5, A6>(constructor: (A0, A1, A2, A3, A4, A5, A6) throws -> T) -> Injector{
+        injector.binder.bind(protocolType, toConstructor: constructor, inScope: scope)
+        return injector
+    }
+    
+    public func to<A0, A1, A2, A3, A4, A5, A6, A7>(constructor: (A0, A1, A2, A3, A4, A5, A6, A7) throws -> T) -> Injector{
+        injector.binder.bind(protocolType, toConstructor: constructor, inScope: scope)
+        return injector
+    }
+    
+    public func to<A0, A1, A2, A3, A4, A5, A6, A7, A8>(constructor: (A0, A1, A2, A3, A4, A5, A6, A7, A8) throws -> T) -> Injector{
+        injector.binder.bind(protocolType, toConstructor: constructor, inScope: scope)
+        return injector
+    }
+    
+    public func to<A0, A1, A2, A3, A4, A5, A6, A7, A8, A9>(constructor: (A0, A1, A2, A3, A4, A5, A6, A7, A8, A9) throws -> T) -> Injector{
+        injector.binder.bind(protocolType, toConstructor: constructor, inScope: scope)
+        return injector
+    }
+    
+    public func to<A0, A1, A2, A3, A4, A5, A6, A7, A8, A9, A10>(constructor: (A0, A1, A2, A3, A4, A5, A6, A7, A8, A9, A10) throws -> T) -> Injector{
+        injector.binder.bind(protocolType, toConstructor: constructor, inScope: scope)
+        return injector
+    }
+    
+    public func to<A0, A1, A2, A3, A4, A5, A6, A7, A8, A9, A10, A11>(constructor: (A0, A1, A2, A3, A4, A5, A6, A7, A8, A9, A10, A11) throws -> T) -> Injector{
+        injector.binder.bind(protocolType, toConstructor: constructor, inScope: scope)
+        return injector
+    }
+    
+    public func to<A0, A1, A2, A3, A4, A5, A6, A7, A8, A9, A10, A11, A12>(constructor: (A0, A1, A2, A3, A4, A5, A6, A7, A8, A9, A10, A11, A12) throws -> T) -> Injector{
+        injector.binder.bind(protocolType, toConstructor: constructor, inScope: scope)
+        return injector
+    }
+    
+    public func to<A0, A1, A2, A3, A4, A5, A6, A7, A8, A9, A10, A11, A12, A13>(constructor: (A0, A1, A2, A3, A4, A5, A6, A7, A8, A9, A10, A11, A12, A13) throws -> T) -> Injector{
+        injector.binder.bind(protocolType, toConstructor: constructor, inScope: scope)
+        return injector
+    }
+    
+    public func to<A0, A1, A2, A3, A4, A5, A6, A7, A8, A9, A10, A11, A12, A13, A14>(constructor: (A0, A1, A2, A3, A4, A5, A6, A7, A8, A9, A10, A11, A12, A13, A14) throws -> T) -> Injector{
+        injector.binder.bind(protocolType, toConstructor: constructor, inScope: scope)
+        return injector
+    }
+    
+    public func to<A0, A1, A2, A3, A4, A5, A6, A7, A8, A9, A10, A11, A12, A13, A14, A15>(constructor: (A0, A1, A2, A3, A4, A5, A6, A7, A8, A9, A10, A11, A12, A13, A14, A15) throws -> T) -> Injector{
+        injector.binder.bind(protocolType, toConstructor: constructor, inScope: scope)
+        return injector
+    }
+    
+    public func to<A0, A1, A2, A3, A4, A5, A6, A7, A8, A9, A10, A11, A12, A13, A14, A15, A16>(constructor: (A0, A1, A2, A3, A4, A5, A6, A7, A8, A9, A10, A11, A12, A13, A14, A15, A16) throws -> T) -> Injector{
+        injector.binder.bind(protocolType, toConstructor: constructor, inScope: scope)
+        return injector
+    }
+    
+    public func to<A0, A1, A2, A3, A4, A5, A6, A7, A8, A9, A10, A11, A12, A13, A14, A15, A16, A17>(constructor: (A0, A1, A2, A3, A4, A5, A6, A7, A8, A9, A10, A11, A12, A13, A14, A15, A16, A17) throws -> T) -> Injector{
+        injector.binder.bind(protocolType, toConstructor: constructor, inScope: scope)
+        return injector
+    }
+    
+    public func to<A0, A1, A2, A3, A4, A5, A6, A7, A8, A9, A10, A11, A12, A13, A14, A15, A16, A17, A18>(constructor: (A0, A1, A2, A3, A4, A5, A6, A7, A8, A9, A10, A11, A12, A13, A14, A15, A16, A17, A18) throws -> T) -> Injector{
+        injector.binder.bind(protocolType, toConstructor: constructor, inScope: scope)
+        return injector
+    }
+    
+    public func to<A0, A1, A2, A3, A4, A5, A6, A7, A8, A9, A10, A11, A12, A13, A14, A15, A16, A17, A18, A19>(constructor: (A0, A1, A2, A3, A4, A5, A6, A7, A8, A9, A10, A11, A12, A13, A14, A15, A16, A17, A18, A19) throws -> T) -> Injector{
+        injector.binder.bind(protocolType, toConstructor: constructor, inScope: scope)
+        return injector
+    }
+    
+    public func to<A0, A1, A2, A3, A4, A5, A6, A7, A8, A9, A10, A11, A12, A13, A14, A15, A16, A17, A18, A19, A20>(constructor: (A0, A1, A2, A3, A4, A5, A6, A7, A8, A9, A10, A11, A12, A13, A14, A15, A16, A17, A18, A19, A20) throws -> T) -> Injector{
+        injector.binder.bind(protocolType, toConstructor: constructor, inScope: scope)
+        return injector
     }
 }
